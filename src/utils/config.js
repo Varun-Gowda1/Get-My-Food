8ff70544eb788753152da9819ba9ad1a434c1028
Varun-Gowda1 <Varungowda2840@gmail.com>
2026-04-01 22:10:54 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,4 +2,4 @@ export const IMG_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_los
 
 export const RESTAURANT_URL ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.4176638&lng=77.7286384&restaurantId=235846&catalog_qa=undefined&submitAction=ENTER"
 
-export const RESTAURANT_LIST_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.3926581&lng=77.7112535&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANT_LIST_URL = "/api/proxy?url=" + encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.3926581&lng=77.7112535&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
